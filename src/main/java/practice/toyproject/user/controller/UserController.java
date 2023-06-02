fa21f80f52425def4b96d0f9a94a2184cbf96a43
LimZida <zida4470@naver.com>
2023-06-02 16:57:37 +0900

    2023 06 02 각주 설명 수정

--- src/main/java/practice/toyproject/user/controller/UserController.java
+++ src/main/java/practice/toyproject/user/controller/UserController.java
@@ -18,7 +18,7 @@ import java.util.List;
  *               saveService(userId) => 유저 저장
  *               selectSeqService(seq) => 시퀀스로 유저 조회
  *               selectAllService() => 모든 유저 조회
- *               selectIdService(userId) => 아이디로 유저 조회
+ *               selectIdService(userId) => ID로 유저 조회
  * reference :
  *
  * author : 임현영
